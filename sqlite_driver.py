f88ee6b87cc5fbf7c2491d22e9a3234b3e9068d1
JcQSteven <523348709@qq.com>
2018-07-13 13:30:05 +0800

    1

--- sqlite_driver.py
+++ sqlite_driver.py
@@ -12,15 +12,70 @@ import sqlite3
 
 class Sqlite_db():
     def __init__(self):
-        self.conn=sqlite3.connect('secrss.sqlite')
-        self.cur=self.conn.cursor()
+        #数据库名字
+        self.db_name ='test.sqlite'
+
+        self.conn=sqlite3.connect(self.db_name)
+        self.cur = self.conn.cursor()
+
         #mysql列名
         #self.mysql_bar='url,title,time_line,tag,author,head,body'
         self.mysql_col_num=8
+
         mysql_col = []
         for i in range(0,self.mysql_col_num):
             mysql_col.append('?')
         self.mysql_col=','.join(mysql_col)
+
+    #返回列数
+    def get_col_num(self,table):
+        self.cur.execute('select * from %s'%table)
+        col_list=[tuple[0] for tuple in self.cur.description]
+        return len(col_list)
+
+    #返回所有列名
+    def get_col_list(self,table):
+        self.cur.execute('select * from %s' % table)
+        col_list = [tuple[0] for tuple in self.cur.description]
+        return col_list
+
+    #执行insert语句时，返回新插入行的ID
+    def insert_id(self):
+        return self.cur.lastrowid()
+
+    #当执行语句时，返回受影响的行数
+    def affected_rows(self):
+        pass
+
+    #上次一查询语句的地方
+    def last_query(self):
+        pass
+
+    #获取数据表总行数
+    def count_all(self,table):
+        self.cur.execute('select * from %s'%table)
+        return len(self.cur.fetchall())
+
+    #执行select语句，获取表的所有数据
+    def get(self,table):
+        self.cur.execute('select * from %s' % table)
+        return self.cur.fetchall()
+
+    def get_where(self,table,**data):
+        #
+        # for a, b in data.items():  # a 代表键 ，b代表值
+        #     print data.items()
+        items= data.items()
+        list=[]
+        for a,b in items:
+            list.append('%s=%s'%(a,b))
+        where= ' and '.join(list)
+
+        sql='select * from %s where %s'%(table,where)
+        print sql
+
+        self.cur.execute('select * from %s where ?'%table,(where,))
+
 
 
     def add(self,table,*data):
@@ -49,4 +104,6 @@ class Sqlite_db():
 if __name__ == '__main__':
 
     sqlite=Sqlite_db()
+    dic={'id': 1, 'a': 'siu'}
+    print sqlite.get_where('test',**dic)
 
